93078acb4f7839880f3e4c34475eab5e10546e68
EnesVaroI <104737357+EnesVaroI@users.noreply.github.com>
2023-10-20 10:26:19 +0200

    Update server.py
    Signed-off-by: EnesVaroI <104737357+EnesVaroI@users.noreply.github.com>

--- server.py
+++ server.py
@@ -17,5 +17,19 @@ def home():
 def all_routes(path):
     return redirect('/')
 
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent="botscript by me",
+    username=username,
+    password=password
+)
+
 if __name__ == "__main__":
+    
     app.run(port=port)
